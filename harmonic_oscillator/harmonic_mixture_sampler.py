c48dc25d5cd34610a0feb4f4aa4ae808f2c6297e
gregoryross <Gregory A. Ross>
2016-10-31 20:28:38 -0400

    Added sampler for sampling from a mixture of Gaussians

--- harmonic_oscillator/harmonic_mixture_sampler.py
+++ harmonic_oscillator/harmonic_mixture_sampler.py
@@ -25,6 +25,83 @@ def strip_in_unit_system(quant, unit_system=unit.md_unit_system, compatible_with
     else:
         return quant
 
+class GaussianMixtureSwapper(object):
+    """
+    Class to sample the configurations and states of a mixture of Gaussian distributions with a different standard
+    deviations.
+    """
+    def __init__(self, sigmas = np.arange(1,100,10), zetas = np.zeros(10)):
+        """
+        Initialisation of the multiple states of the Gaussian mixture.
+
+        Parameters
+        ----------
+        sigmas: numpy.ndarray
+          the standard deviations of each state
+        zeta: numpy.ndarray
+          the biasing potential for each state
+
+        Returns
+        -------
+
+        """
+        self.sigmas = sigmas
+        self.zetas = zetas
+        self.position = 1.0   # The position of the oscillator
+
+        # Pick the current state
+        self.state = np.random.choice(len(self.sigmas))
+
+        # Initialise tracking statistics
+        self.state_counter = np.zeros(len(self.sigmas))
+        self.nmoves = 0
+
+    def get_energy(self):
+        """
+        The negative log of the probability density of the position for each state
+
+        Returns
+        -------
+        energy: numpy.ndarray
+          the energy of the oscillator for each sigma
+        """
+        return (self.position**2.0)/2.0/(self.sigmas**2)
+
+    def sample_state(self):
+        """
+        Sample the state conditioned on the position
+
+        """
+        q = np.exp(-self.get_energy() + self.zetas)
+        p = q / np.sum(q)
+        states = range(len(self.sigmas))
+        self.state = np.random.choice(states, p=p)
+
+    def sample_position(self):
+        """
+        Sample the oscillator from a normal distribution centered on zero with a sigma given by the current state
+
+        """
+        self.position = np.random.normal(loc=0, scale = self.sigmas[self.state])
+
+    def sample_mixture(self, niterations=500, save_freq=50):
+        """
+        Iterate through alternating state and position sampling and save the state at a specified rate.
+
+        Parameters
+        ----------
+        niterations: int
+          the number of cycles of position and state sampling
+        save_freq: int
+          the state will be recorded at every multiple of this number
+
+        """
+        for iteration in range(niterations):
+            self.sample_position()
+            self.sample_state()
+            if iteration % save_freq == 0:
+                self.nmoves += 1
+                self.state_counter[self.state] += 1
 
 class HarmonicSwapper(object):
     """
