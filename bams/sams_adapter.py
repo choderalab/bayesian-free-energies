f936f3d3428144b450a59ae7002fe2944828c791
Gregory Ross <gregoryross.uk@gmail.com>
2017-05-08 17:44:53 -0400

    Added and example to the SAMSAdaptor docstring.

--- bams/sams_adapter.py
+++ bams/sams_adapter.py
@@ -2,8 +2,61 @@ import numpy as np
 
 class SAMSAdaptor(object):
     """
-    Implements the update scheme for self adjusted mixture sampling as described by Z. Tan in Journal of Computational
-    and Graphical Statistics Vol. 26 , Iss. 1, 2017
+    Implements the update scheme for self adjusted mixture sampling as described by Z. Tan in [1]. To function, this
+    class must be paired with a method to perform mixture sampling over states and configurations.
+
+    [1] Journal of Computational and Graphical Statistics Vol. 26 , Iss. 1, 2017
+
+    Example
+    -------
+    Calculating relative free energies of different Gaussian distributions using the Rao-Blackwellized scheme. To
+    simplify things, this example will use the examples_systems.GaussianMixtureSampler to sample over the different
+    Gaussian distributions. We know the analytical free energies for this case, but it serves to demonstrate the
+    basic functionality this class.
+
+    Initialize the distributions you can to calculate the free energies between. All free energies will be calculated
+    relative to the first state (index=0). For the Gaussian example, we'll specify the standard deviations of the
+    distributions, which all have a mean of zero.
+
+    >>> sigms = np.array((1.0, 10.0, 100.))
+    >>> sampler = GaussianMixtureSampler(sigmas=sigmas)
+
+    GaussianMixtureSampler can sample over configurations and states (distributions with different standard deviations
+    in this case). To use this class for your own cases, you'll have to write your own mixture sampler.
+
+    Next, we'll initialize this class, which will estimates given samples from states.
+
+    >>> adaptor = SAMSAdaptor(nstates=len(sigmas), two_stage=True, flat_hist=0.2)
+
+    With the two_stage flag set to True, a burn-in scheme is performed as described in equation 15 of [1]. The burn-in
+    stage will finish when the state count histogram is within 20% (flat_hist=0.2) of the target_weights. By default,
+    the target_weights are uniform over the states.
+
+    The SAMSAdaptor works by tracking the state of the sampler and updating the bias accordingly. The bias is used to
+    sample from the different states at the target probability. There are 4 main stages to calculating free energies
+    with this class. The example below performs 50 iterations of mixture sampling and SAMS updates and each stage is
+    labeled and described below.
+
+    >>> for i in range(500):
+    >>>     sampler.step()                 # 1.
+    >>>     noisy = sampler.weights        # 2.
+    >>>     z = -adaptor.update(state=sampler.state , noisy_observation=noisy, histogram=sampler.histogram)     # 3.
+    >>>     sampler.zetas = z              # 4.
+
+    In stage 1., states and configurations are sampled over with GaussianMixtureSampler. In stage 2., the Rao-
+    Blackwellized weights (which are proportional to the conditional probability of being sampled from one the
+    Gaussians given the configuration) are calculated. If instead of the Rao-Blackwellized scheme, the SAMS binary
+    scheme was being used, the 'noisy' variable would be a binary vector with the only 1 at the index of the current
+    state. In stage 3., the current state of the mixture sampler ('sampler.state', which is either 0, 1, 2 in this
+    case), the noisy observation, and histogram of the state counts is supplied to the SAMS object. The state counts are
+    not essential here, but are necessary if you want to use the two-stage scheme. The new bias required to achieve the
+    target weights for that iteration is returned. In stage 4., the bias is supplied to the sampler. Thus, the next
+    iteration samples from the mixture with an updated state bias.
+
+    As the number of iterations tends to infinity, the bias will converge to a value that can be used as an unbiased
+    estimate of the free energies. When the target weights are equal, as they are above, the bias tends to the relative
+    free energies of the Gaussian probability distributions.
+
     """
     def __init__(self, nstates, zetas=None, target_weights=None, two_stage=True, beta=0.6, flat_hist=0.2):
         """
